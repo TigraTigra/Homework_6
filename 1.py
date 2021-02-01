ffa8a8dd92f545728cc3984bc302dc2e1f4b5f4f
TigraTigra <pavelremarque@gmail.com>
2021-02-01 23:11:16 +0300

    Решение

--- 1.py
+++ 1.py
@@ -8,3 +8,25 @@
 # Задачу можно усложнить, реализовав проверку порядка режимов,
 # и при его нарушении выводить соответствующее сообщение и завершать скрипт.
 
+from time import sleep
+
+
+class TrafficLight:
+    __color = ['Красный', 'Желтый', 'Зеленый']
+
+    def running(self):
+        i = 0
+        while i < 3:
+            print(f'Светофор переключается \n '
+                  f'{TrafficLight.__color[i]}')
+            if i == 0:
+                sleep(7)
+            elif i == 1:
+                sleep(5)
+            elif i == 2:
+                sleep(3)
+            i += 1
+
+
+TrafficLight = TrafficLight()
+TrafficLight.running()
